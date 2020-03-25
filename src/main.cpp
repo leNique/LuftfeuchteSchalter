832e6253662e6029ea6ee3f9949ac9ba5b63baab
leNique <lot_nik@gmx.de>
2020-03-25 12:05:45 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -19,9 +19,18 @@ TM1637Display display = TM1637Display(CLK, DIO);
 unsigned long TimerSensor=0;
 unsigned long TimerBlink=0;
 
+unsigned long Betriebstunden=0;
+unsigned long BetriebstundenEinMerker=0;
+
+
 byte DisplayStatus=0;
 bool TasterMerker=0;
 byte Blink=0;
+bool IstEin=0;
+
+
+int absulutInnen=0;
+int absulutAussen=0;
 
 
 // Create array that turns all segments on:
@@ -110,6 +119,28 @@ if (millis()<TimerBlink)
    //SensorenLesen 
    sensorInnen.Read();
    sensorAussen.Read();
+    
+   // Absulute Feuchten berechnen und Relais Schalten
+    // die letzten 2 Stellen sind Nachkommastellen
+    absulutInnen = (13,233*sensorInnen.h*((10^((7,5*sensorInnen.t)/(237+sensorInnen.t)))/(273,16+sensorAussen.t)))*100;
+    absulutAussen = (13,233*sensorAussen.h*((10^((7,5*sensorAussen.t)/(237+sensorAussen.t)))/(273,16+sensorInnen.t)))*100;
+    
+    if (absulutInnen-50>absulutAussen && IstEin==0) 
+     { //Einschalten
+      
+      BetriebstundenEinMerker=millis();
+      IstEin=1;
+     } 
+    if (absulutInnen-10<absulutAussen && IstEin==1)
+     { //Ausschalten
+      
+      if (millis()>BetriebstundenEinMerker) // Wenn millis() überläuft wird diese Betriebszeit einfach ignoriert
+       {Betriebstunden=Betriebstunden+(millis()-BetriebstundenEinMerker);}
+      if (Betriebstunden>35996400000‬) // Wenn die Betriebsstunden 9999 Stunden überschreiten werden Sie auf 0 gesetzt
+       {Betriebstunden=0;}
+      IstEin=0;
+     } 
+      
    TimerSensor=millis();
   }  
   
