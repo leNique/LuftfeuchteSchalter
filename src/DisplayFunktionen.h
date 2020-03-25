6a25f6534cf6d53038a47119f10e3f1172dbb85f
leNique <lot_nik@gmx.de>
2020-03-25 13:17:18 +0100

    Create DisplayFunktionen.h

---
+++ src/DisplayFunktionen.h
@@ -0,0 +1,9 @@
+#ifndef DISPLAYFUNKTIONEN_H
+	#define DISPLAYFUNKTIONEN_H
+ 
+	void DisplayTemp(byte);
+ void DisplayHumidityR(byte);
+ void DisplayHumidityA(byte);
+ void DisplayBetrieb(void);
+ 
+#endif
