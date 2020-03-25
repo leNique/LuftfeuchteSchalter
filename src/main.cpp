066aa096b9aaf7c26f77ec2df7b670e4537edafa
leNique <lot_nik@gmx.de>
2020-03-25 09:52:56 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 #include <TM1637Display.h>
 
+#include <SoftwareWire.h>
+#include <AM2320.h>
+
 //Temp_LuftfSenor - AM2320
-#include <AM232X.h>
-AM232X AM2320;
+AM2320 sensorInnen(5,6); // AM2320 sensor attached SDA to digital PIN 5 and SCL to digital PIN 6
+AM2320 sensorAussen(9,10); // AM2320 sensor attached SDA to digital PIN 9 and SCL to digital PIN 10
 
 
 #define CLK 11
@@ -11,6 +14,13 @@ AM232X AM2320;
 #define PinSchalter 5
 
 TM1637Display display = TM1637Display(CLK, DIO);
+
+
+unsigned long TimerSensor=0;
+
+
+byte DisplayStatus=0;
+bool TasterMerker=0;
 
 
 // Create array that turns all segments on:
@@ -34,21 +44,74 @@ const uint8_t celsius[] = {
 
 
 void setup() {
-  // Clear the display:
 pinMode(PinSchalter, INPUT);
+  
+  // Clear the display:
  display.clear();
- delay(1000);
+ delay(500);
  display.setBrightness(7);
+ display.setSegments(data);
+ delay(1000);
+  
 }
 
 void loop() {
 
-if (digitalRead(5))
-   {display.setSegments(done);}
+  //Timer nach Überlauf setzen
+if (millis()<TimerSensor)
+ {TimerSensor=millis();}
+  
+  
+  // 1. Mal pro Minute ausführen
+  if (TimerSensor+60000<millis())
+  {
+   //SensorenLesen 
+   sensorInnen.Read();
+   sensorAussen.Read();
+   TimerSensor=millis();
+  }  
+  
+// Taster lesen und DisplayStatus anpassen  
+if (digitalRead(5) && TasterMerker==0)
+   { 
+    DisplayStatus++;
+    if (DisplayStatus>6)
+     {DisplayStatus=0;}
+    TasterMerker=1;
+   }
 else
+    {
+     TasterMerker=0;
+    }
 
-    {display.clear();}
 
-
+  
+ // ----- Display ------ 
+  
+  switch (var) {
+      case 0:
+             display.clear();
+      break;
+      case 1:
+      DisplayTemp(1);
+      break;
+      case 2:
+      DisplayHumidityR(1);
+      break;
+      case 3:
+      DisplayHumidityA(1);
+      break;
+      case 4:
+      DisplayTemp(2);
+      break;
+      case 5:
+      DisplayHumidityR(2);
+      break;
+      case 6:
+      DisplayHumidityR(2);
+      break;
+  }
+  
+  
 
 }
