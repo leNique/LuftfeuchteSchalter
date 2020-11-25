64060dd4b3285f070781317ce4d1d557012c2510
leNique <lot_nik@gmx.de>
2020-11-25 15:20:11 +0100

    Update install_arduino.sh

--- .circleci/install_arduino.sh
+++ .circleci/install_arduino.sh
@@ -2,3 +2,6 @@ wget http://downloads.arduino.cc/arduino-1.8.8-linux64.tar.xz
 tar xf arduino-1.8.8-linux64.tar.xz
 sudo mv arduino-1.8.8 /usr/local/share/arduino
 sudo ln -s /usr/local/share/arduino/arduino /usr/local/bin/arduino
+
+
+arduino lib install "TM1637" "AM232X"
