d3cb0df4a66237b2ef6c3cec488c480578507c76
leNique <lot_nik@gmx.de>
2020-11-25 17:29:08 +0100

    Update install_arduino.sh

--- .circleci/install_arduino.sh
+++ .circleci/install_arduino.sh
@@ -17,7 +17,7 @@ sudo ln -s /usr/local/share/arduino-cli /usr/local/bin/arduino-cli
 ##    - https://dl.espressif.com/dl/package_esp32_index.json" >> .cli-config.yml
 printf "directories:
 data:  
-- $PWD/lib" >> .cli-config.yml
+- ./lib" >> .cli-config.yml
 sudo mv .cli-config.yml /usr/local/share/
 
 arduino-cli core update-index
