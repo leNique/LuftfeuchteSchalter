7d46b2be8bd044381a08a16933042de956187bf7
leNique <lot_nik@gmx.de>
2020-11-25 15:51:40 +0100

    Update install_arduino.sh

--- .circleci/install_arduino.sh
+++ .circleci/install_arduino.sh
@@ -26,10 +26,10 @@ arduino-cli lib install "TM1637" "AM232X"
 
 
 
-wget http://downloads.arduino.cc/arduino-1.8.8-linux64.tar.xz
-tar xf arduino-1.8.8-linux64.tar.xz
-sudo mv arduino-1.8.8 /usr/local/share/arduino
-sudo ln -s /usr/local/share/arduino/arduino /usr/local/bin/arduino
+##wget http://downloads.arduino.cc/arduino-1.8.8-linux64.tar.xz
+##tar xf arduino-1.8.8-linux64.tar.xz
+##sudo mv arduino-1.8.8 /usr/local/share/arduino
+##sudo ln -s /usr/local/share/arduino/arduino /usr/local/bin/arduino
 
 
-arduino lib install "TM1637" "AM232X"
+##arduino lib install "TM1637" "AM232X"
