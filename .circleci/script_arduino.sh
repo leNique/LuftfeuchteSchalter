82f3995ce4c321a91f9a4da74c752ab1f4bccb2b
leNique <lot_nik@gmx.de>
2020-11-25 14:37:27 +0100

    Update script_arduino.sh

--- .circleci/script_arduino.sh
+++ .circleci/script_arduino.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-arduino-cli compile --output temp.bin -b esp32:esp32:esp32 $PWD/Test/Test.ino --debug
+arduino-cli compile --output temp.bin -b esp32:esp32:esp32 $PWD/src/main.cpp --debug
