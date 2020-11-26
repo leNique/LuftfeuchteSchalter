4ba2ad505ddeaaa45ee45a5b7a5daf8c722ec6ad
leNique <lot_nik@gmx.de>
2020-11-26 08:33:52 +0100

    Update script_arduino.sh

--- .circleci/script_arduino.sh
+++ .circleci/script_arduino.sh
@@ -7,6 +7,6 @@ arduino-cli compile --output temp.bin -b arduino:avr:uno $PWD/src/main.cpp --deb
 
 
 arduino-cli config dump
-
+dir $PWD/src/libraries
 
 ##arduino --verify --board arduino:avr:uno $PWD/src/main.cpp
